018db6dff9ef22aa2bc391b0218e20129fd09251
tailor <dann.croitoru@gmail.com>
2013-02-10 22:28:57 +0200

    provide link in shell

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+"""Lazy4Chan  
+
+Usage:
+  l4c.py LINK
+
+"""
 from bs4 import BeautifulSoup
 import urllib, urllib2
 from urlparse import urlparse
 import re
-from datetime import datetime
 import os
+from docopt import docopt
 
-link='http://boards.4chan.org/b/res/456353246'
 
 def getSoup(link=''):
     print 'Accesing ', link
@@ -15,22 +20,26 @@ def getSoup(link=''):
     return BeautifulSoup(html)
 
 if __name__ == '__main__':
-    print 'Getting page HTML...'
-    soup = getSoup(link)
-    folder =  urlparse(link).path.split('/')[-1]
-    if not os.path.exists(folder):
-    	os.makedirs(folder)
-    i = 0
-    for span in soup.find_all('span',{'class':'fileText'}):
-    	imgLink = re.sub('//','http://',span.find('a')['href'])
-    	name = urlparse(imgLink).path.split('/')[-1]
-    	img = urllib2.urlopen(imgLink).read()
+    arguments = docopt(__doc__)
+    link =  arguments['LINK']
+    try:
+        soup = getSoup(link)
+        folder =  urlparse(link).path.split('/')[-1]
+        if not os.path.exists(folder):
+        	os.makedirs(folder)
+        i = 0
+        for span in soup.find_all('span',{'class':'fileText'}):
+        	imgLink = re.sub('//','http://',span.find('a')['href'])
+        	name = urlparse(imgLink).path.split('/')[-1]
+        	img = urllib2.urlopen(imgLink).read()
 
-    	file = open(os.path.join(folder,name),'w+')
-    	file.write(img)
-    	file.close
-    	i += 1
-    	print name
+        	file = open(os.path.join(folder,name),'w+')
+        	file.write(img)
+        	file.close
+        	i += 1
+        	print name
 
-    print 'Added %i filed' % i
+        print 'Added %i files' % i
+    except:
+        print 'No valid link provided'
 
